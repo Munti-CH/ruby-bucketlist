b63901e6022298c8cd11617801c10fbaa40d1b79
Munti-CH <51264438+Munti-CH@users.noreply.github.com>
2020-04-26 18:13:52 +0200

    Extending an existing ideas - Task 5: Add photo_url attribute to Idea model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200426154910) do
+ActiveRecord::Schema.define(version: 20200426161204) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "done_count"
+    t.string "photo_url"
   end
 
 end
